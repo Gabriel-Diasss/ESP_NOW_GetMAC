70db6bd338e975e8e8fe8f93585f201c0ea2f9ad
Gabriel Dias <g248134@dac.unicamp.br>
2026-05-27 20:31:43 -0300

    Add dual-environment support for ESP32 and ESP8266

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,7 @@
   ESP32 MAC Address printout
   esp32-mac-address.ino
   Prints MAC Address to Serial Monitor
-
-  DroneBot Workshop 2022
-  https://dronebotworkshop.com
-
-[env:esp32dev]
-platform = espressif32
-// para rodar no esp8266, troque as linhas do .ini de board e platform para as seguintes:
-#espressif8266
-#esp12e
+  Created by Gabriel Dias.
   */
 
 // Include WiFi Library
